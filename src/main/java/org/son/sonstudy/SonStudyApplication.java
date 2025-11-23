76364821fb9e0cc4ebcbb4c56cdecd2906480f0b
chounjae <blueskycuj@naver.com>
2025-11-23 21:26:06 +0900

    Initial commit

---
+++ src/main/java/org/son/sonstudy/SonStudyApplication.java
@@ -0,0 +1,13 @@
+package org.son.sonstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/son/sonstudy/SonStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.son.sonstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
