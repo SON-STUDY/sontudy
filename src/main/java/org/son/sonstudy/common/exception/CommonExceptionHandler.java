0215c3f2059df409907a9462e61f0497983ff496
leedy5521 <leedy5521@naver.com>
2026-01-02 18:44:39 +0900

    feat: 검증 예외 처리 핸들러 로직 추가

--- src/main/java/org/son/sonstudy/common/exception/CommonExceptionHandler.java
+++ src/main/java/org/son/sonstudy/common/exception/CommonExceptionHandler.java
@@ -1,7 +1,10 @@
 package org.son.sonstudy.common.exception;
 
+import org.son.sonstudy.common.api.code.ErrorCode;
 import org.son.sonstudy.common.api.response.ApiResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +14,13 @@ public class CommonExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ApiResponse<Void>> handleCustomException(CustomException e){
         return ApiResponse.fail(e.getErrorCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Void>> handleValidationException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String firstErrorMessage = bindingResult.getAllErrors().get(0).getDefaultMessage();
+
+        return ApiResponse.fail(ErrorCode.BAD_REQUEST, firstErrorMessage);
     }
 }
