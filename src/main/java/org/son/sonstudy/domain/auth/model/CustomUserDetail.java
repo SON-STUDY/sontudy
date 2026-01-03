5aacf0f715fc80cb6ce88bc638fdbe549927bea3
chounjae <blueskycuj@naver.com>
2026-01-03 12:21:11 +0900

    fix: 추상화된 인증 객체 반환

--- src/main/java/org/son/sonstudy/domain/auth/model/CustomUserDetail.java
+++ src/main/java/org/son/sonstudy/domain/auth/model/CustomUserDetail.java
@@ -1,6 +1,7 @@
 package org.son.sonstudy.domain.auth.model;
 
 import lombok.RequiredArgsConstructor;
+import org.son.sonstudy.common.jwt.UserContext;
 import org.son.sonstudy.domain.user.model.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -31,6 +32,14 @@ public class CustomUserDetail implements UserDetails {
     @Override
     public String getUsername() {
         return this.user.getEmail();
+    }
+
+    public UserContext toUserContext() {
+        return new UserContext(
+                user.getId(),
+                user.getEmail(),
+                user.getRole()
+        );
     }
 
     @Override
