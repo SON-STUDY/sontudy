280fb30ec90d1fe070cd639427790ed82a611f3a
leedy5521 <leedy5521@naver.com>
2026-01-03 18:23:24 +0900

    test: 서비스 테스트 코드 작성(서비스,레포지토리 통합테스트)

--- src/main/java/org/son/sonstudy/domain/product/model/submodel/Color.java
+++ src/main/java/org/son/sonstudy/domain/product/model/submodel/Color.java
@@ -2,10 +2,12 @@ package org.son.sonstudy.domain.product.model.submodel;
 
 import jakarta.persistence.*;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "color")
+@Getter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Color {
     @Id
