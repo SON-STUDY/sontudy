d74d2a49ed2575ac18c5c2461dbd968258c955c0
leedy5521 <leedy5521@naver.com>
2026-01-29 21:56:35 +0900

    feat: getCurrentUser()유틸 메서드 기능 구현

--- src/main/java/org/son/sonstudy/common/util/SecurityUtils.java
+++ src/main/java/org/son/sonstudy/common/util/SecurityUtils.java
@@ -1,11 +1,30 @@
 package org.son.sonstudy.common.util;
 
+import org.son.sonstudy.domain.auth.model.CustomUserDetail;
 import org.son.sonstudy.domain.user.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public class SecurityUtils {
 
     public static User getCurrentUser() {
-        // ToDo: 현재 로그인 유저 객체 반환하는 유틸 메서드 구현
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof CustomUserDetail customUserDetail) {
+            return customUserDetail.getUser();
+        }
+        if (principal instanceof User) {
+            return (User) principal;
+        }
+        if (principal instanceof UserDetails userDetails && userDetails instanceof CustomUserDetail) {
+            return ((CustomUserDetail) userDetails).getUser();
+        }
+
         return null;
     }
 }
