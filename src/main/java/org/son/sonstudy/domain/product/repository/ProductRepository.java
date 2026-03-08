f5da19f4b879fc0abfe8f49df31daa538f6c2cd2
hefour <jhec0113@naver.com>
2026-03-08 14:30:58 +0900

    feat: 브랜별 상품 조회 기능 추가

--- src/main/java/org/son/sonstudy/domain/product/repository/ProductRepository.java
+++ src/main/java/org/son/sonstudy/domain/product/repository/ProductRepository.java
@@ -2,6 +2,9 @@ package org.son.sonstudy.domain.product.repository;
 
 import org.son.sonstudy.domain.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ProductRepository extends JpaRepository<Product, String>, ProductRepositoryCustom {
+    Page<Product> findByBrandIgnoreCase(String brand, Pageable pageable);
 }
