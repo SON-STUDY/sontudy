ce28303dbfea6627c7e89f91d6977faf82b7626e
hefour <jhec0113@naver.com>
2026-03-11 11:42:49 +0900

    fix: status가 ON_SALE인 경우만 검색되도록 수정

--- src/main/java/org/son/sonstudy/domain/product/repository/ProductRepository.java
+++ src/main/java/org/son/sonstudy/domain/product/repository/ProductRepository.java
@@ -1,10 +1,19 @@
 package org.son.sonstudy.domain.product.repository;
 
 import org.son.sonstudy.domain.product.model.Product;
+import org.son.sonstudy.domain.product.model.ProductStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, String>, ProductRepositoryCustom {
-    Page<Product> findByBrandIgnoreCase(String brand, Pageable pageable);
+    @Query("""
+            select p
+            from Product p
+            where upper(p.brand) = upper(:brand)
+            and p.status = :status
+            """)
+    Page<Product> findByBrandIgnoreCase(@Param("brand") String brand, @Param("status") ProductStatus status, Pageable pageable);
 }
