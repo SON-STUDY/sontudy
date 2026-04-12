b65f0b64006884563062fe2dcd386500b7fccc8f
leedy5521 <leedy5521@naver.com>
2026-04-12 20:21:33 +0900

    feat: 에러코드, 도메인 메서드(registerShipment) 추가

--- src/main/java/org/son/sonstudy/domain/delivery/model/Delivery.java
+++ src/main/java/org/son/sonstudy/domain/delivery/model/Delivery.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.son.sonstudy.common.api.code.ErrorCode;
+import org.son.sonstudy.common.exception.CustomException;
 
 @Entity
 @Getter
@@ -30,5 +32,14 @@ public class Delivery {
         Delivery delivery = new Delivery();
         delivery.status = DeliveryStatus.READY;
         return delivery;
+    }
+
+    public void registerShipment(String courierCompany, String trackingNumber) {
+        if (this.status != DeliveryStatus.READY) {
+            throw new CustomException(ErrorCode.INVALID_DELIVERY_STATUS);
+        }
+        this.courierCompany = courierCompany;
+        this.trackingNumber = trackingNumber;
+        this.status = DeliveryStatus.DELIVERING;
     }
 }
